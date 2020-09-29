ff5b31864cdb682ea7887467f216719180c54a8c
Vresod <lukebinkofsky@gmail.com>
2020-09-29 10:20:13 -0400

    added disclaimers about """"stolen"""" code

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+
+# a lot of this code was stolen from AVCADO/KrakenBot. infact, this was originally made as a bot that was kraken but without everything but the rolldice function
+# avcado please dont yell at me
+# thanks
+
 import discord
 import random
 from discord.ext import commands
@@ -10,7 +15,7 @@ client = commands.Bot(command_prefix="d!")
 client.remove_command("help")
 
 @client.event
-async def on_ready():
+async def on_ready(): # i stole the idea for printing the guilds and the invite link from kraken. i (vresod) wrote that code so it should be fine
     print("bot is ready")
     print(f"https://discord.com/oauth2/authorize?client_id={client.user.id}&permissions=0&scope=bot")
     for guild in client.guilds:
@@ -20,7 +25,7 @@ async def on_ready():
 async def on_guild_join(guild):
     print(f"Joined guild: {guild.name}")
 
-@client.command()
+@client.command() # i also took this from kraken
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     dice = [
         str(random.choice(range(1, number_of_sides + 1)))
